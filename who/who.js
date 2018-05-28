46b1d2d84344ec12b2d628e53942ceefe0b6094c
Harmeet Singh Saluja <harmeet.saluja@yash.com>
2018-05-28 18:55:23 +0530

    who.js created

---
+++ who/who.js
@@ -0,0 +1,17 @@
+function Who(){
+	// this.name=function(name){
+	// 	if(name==undefined)
+	// 		return 'One for you, one for me.';
+	// 	else
+	// 		return 'One for '+name+', one for me.';
+	// }
+}
+
+Who.prototype.name=function(name){
+		if(name==undefined)
+			return 'One for you, one for me.';
+		else
+			return 'One for '+name+', one for me.';
+	}
+
+module.exports=Who;
